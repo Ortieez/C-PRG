e497caef8f67e397b371539eb77298d3efa0a0b6
Adam Lipert <a.lipert@protonmail.com>
2021-02-22 19:42:01 +0100

    Deleted File and Made the homework

--- Homeworks/ArrayAnalyze/analyza-pole.c
+++ Homeworks/ArrayAnalyze/analyza-pole.c
@@ -5,27 +5,49 @@
 
 int minmax(int *arr, int size)
 {
-    int position = 0, value = arr[position];
+    int minposition = 0, minvalue = arr[minposition];
+    int maxposition = 0, maxvalue = arr[maxposition];
+    int position = 0, num = arr[position];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < minvalue)
+        {
+            minvalue = arr[i];
+            minposition = i;
+        }
+        if (arr[i] > maxvalue)
+        {
+            maxvalue = arr[i];
+            maxposition = i;
+        }
+    }
+    printf("Minimal Value in array => [%i => %03i]\n", minposition, minvalue);
+    printf("Maximum Value in array => [%i => %03i]\n", maxposition, maxvalue);
+
+    int x = 0;
+    for (int i = 0; i < size; i++)
+    {
+        x += arr[i];
+        position = i;
+    }
+    float z = (x - (maxvalue + minvalue)) / (float)size;
+    printf("Average number of this array without min and max is => %.2f\n", z);
+}
+
+int countAll(int *arr, int size)
+{
+    int position = 0, num = arr[position], x = 0;
 
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] < value)
-        {
-            value = arr[i];
-            position = i;
-        }
+        x = x + arr[i];
     }
-    printf("Minimal Value in array => [%i => %03i]\n", position, value);
+    printf("Sum of this array is => %i\n", x);
 
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] > value)
-        {
-            value = arr[i];
-            position = i;
-        }
-    }
-    printf("Maximum Value in array => [%i => %03i]\n", position, value);
+    float y = (float)x / (float)size;
+    printf("Average number of this array is => %.2f\n", y);
+
+    // printf("Average number of this array without min and max is => %.2f\n", z);
 }
 
 int main()
@@ -44,6 +66,7 @@ int main()
 
     // Analyze
     minmax(array, sizeof(array) / 4);
+    countAll(array, sizeof(array) / 4);
 
     return 0;
 }
